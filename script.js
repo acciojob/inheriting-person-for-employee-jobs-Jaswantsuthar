e723bdc96184f45e3e8f794e0b181bc37be04999
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-07-20 09:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+function createPerson(name, age) {
+  return {
+    name,
+    age,
+    greet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+  };
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+function createEmployee(name, age, jobTitle) {
+  const person = createPerson(name, age);
+  return {
+    ...person,
+    jobTitle,
+    jobGreet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+  };
 }
 
 // Do not change code below this line
